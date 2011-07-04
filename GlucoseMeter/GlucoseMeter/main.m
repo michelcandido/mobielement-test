07cac2df73899706ee1d80484939faf68b7aa3b7
zhouzhin <zhouzhin@mobielement.com>
2011-07-04 06:35:40 +0000

    init commit

---
+++ GlucoseMeter/GlucoseMeter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GlucoseMeter
+//
+//  Created by Tony on 7/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
