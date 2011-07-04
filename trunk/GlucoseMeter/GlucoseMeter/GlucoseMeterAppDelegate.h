d8c9292bdece2a19ba5811a9866d626ec7175302
zhouzhin@mobielement.com <zhouzhin@mobielement.com@e03c42f4-232f-451b-ce6c-4111afdebb9d>
2011-07-04 06:35:40 +0000

    init commit

---
+++ trunk/GlucoseMeter/GlucoseMeter/GlucoseMeterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GlucoseMeterAppDelegate.h
+//  GlucoseMeter
+//
+//  Created by Tony on 7/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GlucoseMeterAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
