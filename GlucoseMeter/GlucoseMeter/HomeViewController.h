73ff716fc15218136b6bb035a29fe70295d676d0
zhouzhin <zhouzhin@mobielement.com>
2011-07-08 07:07:42 +0000

    switch between different views

---
+++ GlucoseMeter/GlucoseMeter/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  GlucoseMeter
+//
+//  Created by Tony on 7/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
