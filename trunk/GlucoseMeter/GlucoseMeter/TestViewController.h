293028a71b07bcee0a22a5a0a8d1947af17c29ec
zhouzhin@mobielement.com <zhouzhin@mobielement.com@e03c42f4-232f-451b-ce6c-4111afdebb9d>
2011-07-12 15:25:26 +0000

    basic layout for testing setup

--- trunk/GlucoseMeter/GlucoseMeter/TestViewController.h
+++ trunk/GlucoseMeter/GlucoseMeter/TestViewController.h
@@ -8,6 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TestViewController : UIViewController
+#define kStepTag 1
+#define kInstructionTag 2
 
+@interface TestViewController : UIViewController
+<UITableViewDelegate, UITableViewDataSource> 
+{
+    NSArray *testInstructions; 
+    UITableViewCell *tvCell;
+} 
+@property (nonatomic, retain) NSArray *testInstructions;
+@property (nonatomic, retain) IBOutlet UITableViewCell *tvCell;
+-(IBAction)readyBtnTapped:(id)sender;
 @end
