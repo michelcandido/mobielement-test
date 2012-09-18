d80320faa0e7e9a338c2e73c30b7e51c6ed601d2
zhouzhin <zhouzhin@mobielement.com>
2012-09-18 07:18:59 +0000

    register/unregister device logger

--- StarSightings/StarSightings/MainPage.xaml.cs
+++ StarSightings/StarSightings/MainPage.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.Text;
+using StarSightings.Events;
 
 namespace StarSightings
 {
@@ -58,6 +60,57 @@ namespace StarSightings
             if (selectedItemData != null)
             {
                 this.NavigationService.Navigate(new Uri(string.Format("/DetailsPage.xaml?selectedItemId={0}", selectedItemData.ID), UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        private void DoTest(object sender, System.Windows.Input.GestureEventArgs e)
+        {/*
+            WebClient webClient = new WebClient();
+            string auth = Constants.BASE_AUTH_USERNAME + ":" + Constants.BASE_AUTH_PASSWORD;
+            string authString = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(auth));
+
+            webClient.Headers["Authorization"] = "Basic " + authString;
+            webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(WebClient_DownloadStringCompleted);
+            //webClient.DownloadStringAsync(new System.Uri("http://test.starsightings.com/index.php?mobile=1&start=0&limit=15&search_types="));            
+            //webClient.DownloadStringAsync(new System.Uri("http://test.starsightings.com/index.php?page=device&mode=register&mobile=1&device_id=1_1212121212&device_token=att"));      
+            UriBuilder baseUri = new UriBuilder("http://test.starsightings.com/index.php");
+            string paras = "page=device&mode=unregister&mobile=1&device_id=1_1212121212";
+            baseUri.Query = paras;
+            webClient.DownloadStringAsync(baseUri.Uri);
+
+            */
+            App.SSAPI.Register += new RegisterEventHandler(RegisterDeviceCompleted);
+            App.SSAPI.UnregisterDevice();
+        }
+
+        public void RegisterDeviceCompleted(object sender, RegisterEventArgs e)
+        {
+            MessageBox.Show(e.Successful+":"+e.DeviceId);
+        }
+
+        private void WebClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    // Showing the exact error message is useful for debugging. In a finalized application, 
+                    // output a friendly and applicable string to the user instead. 
+                    MessageBox.Show(e.Error.Message);
+                });
+            }
+            else
+            {
+                // Save the feed into the State property in case the application is tombstoned. 
+                this.State["feed"] = e.Result;
+
+                Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    // Showing the exact error message is useful for debugging. In a finalized application, 
+                    // output a friendly and applicable string to the user instead. 
+                    MessageBox.Show(e.Result);
+                });
+                //UpdateFeedList(e.Result);
             }
         }
     }
