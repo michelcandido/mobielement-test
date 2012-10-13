7351737005a8193398306c4e1ddea07556627cdc
zhouzhin@mobielement.com <zhouzhin@mobielement.com@e03c42f4-232f-451b-ce6c-4111afdebb9d>
2012-10-13 05:41:34 +0000

    enable image download for test server by using converter

---
+++ trunk/StarSightings/StarSightings/Converters/ImageConverter.cs
@@ -0,0 +1,54 @@
+﻿#define TESTSERVER
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows.Media.Imaging;
+using System.IO;
+
+namespace StarSightings.Converters
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            BitmapImage bmp = new BitmapImage();
+            if (value == null)
+                return bmp;
+            System.Net.HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(value.ToString());
+            //request.CookieContainer = App.CookieJar;
+            request.Method = "GET";
+#if (TESTSERVER)            
+                string auth = Constants.BASE_AUTH_USERNAME + ":" + Constants.BASE_AUTH_PASSWORD;
+                string authString = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(auth));
+                request.Headers["Authorization"] = "Basic " + authString;            
+#endif
+            request.BeginGetResponse((s) =>
+            {
+                HttpWebRequest requestResult = (HttpWebRequest)s.AsyncState;
+                HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(s);
+                Stream content = response.GetResponseStream();
+                System.Windows.Deployment.Current.Dispatcher.BeginInvoke(() =>
+                {
+                    bmp.SetSource(content);
+                });
+            }, request);
+
+            return bmp;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        } 
+    }
+}
