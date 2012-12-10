e6e13ef6654aca2b56e044af335a2619d1b5a9c6
zhouzhin@mobielement.com <zhouzhin@mobielement.com@e03c42f4-232f-451b-ce6c-4111afdebb9d>
2012-12-10 08:39:22 +0000

    implementation comment list page

---
+++ trunk/StarSightings/StarSightings/VoteCommentPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
+
+namespace StarSightings
+{
+    public partial class VoteCommentPage : PhoneApplicationPage
+    {
+        public VoteCommentPage()
+        {
+            InitializeComponent();
+            DataContext = App.ViewModel;
+        }
+
+        private void htmlTextBlock_NavigationRequested(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            WebBrowserTask webBrowserTask = new WebBrowserTask();
+
+            webBrowserTask.Uri = new Uri(((e.Content as Hyperlink).CommandParameter as string), UriKind.Absolute);
+
+            webBrowserTask.Show();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (e.NavigationMode != System.Windows.Navigation.NavigationMode.Back)
+            {
+                if (NavigationContext.QueryString.ContainsKey("pivotItem"))
+                {
+                    string pivotItem = NavigationContext.QueryString["pivotItem"];
+                    if (pivotItem == "comment")
+                        this.pivotControl.SelectedIndex = 1;                    
+                }
+            }            
+        }
+    }
+}
