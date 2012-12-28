4d0243283467da59aef8372e82515fc2fd69c5bb
zhouzhin <zhouzhin@mobielement.com>
2012-12-28 06:49:22 +0000

    UI change on scoop page

--- StarSightings/StarSightings/CameraMode.xaml.cs
+++ StarSightings/StarSightings/CameraMode.xaml.cs
@@ -40,8 +40,7 @@ namespace StarSightings
         }
 
         private void TakePictureButton_Click(object sender, RoutedEventArgs e)
-        {
-            // this.NavigationService.Navigate(new Uri("/CameraPage.xaml", UriKind.RelativeOrAbsolute));
+        {            
             try
             {
                 cameraCaptureTask.Show();
@@ -67,38 +66,7 @@ namespace StarSightings
 
         private void photoChooserTask_Completed(object sender, PhotoResult e)
         {
-            //var frame = Application.Current.RootVisual as PhoneApplicationFrame;
-            //var startPage = frame.Content as PhoneApplicationPage;
-            /*
-            TaskEventArgs<PhotoResult> photo = e as TaskEventArgs<PhotoResult>;
-            if (photo != null && photo.Result != null && photo.Result.ChosenPhoto != null)
-            {
-            BitmapImage image = new BitmapImage();
-            image.SetSource(photo.Result.ChosenPhoto);
-            SelectedPicturePage.
-            imgPhoto.Source = image;
-            }*/
-            /*
-            base.Completed(sender, e);
-            TaskEventArgs<PhotoResult> photo = e as TaskEventArgs<PhotoResult>;
-            if (photo != null && photo.Result != null && photo.Result.ChosenPhoto != null)
-            {
-            BitmapImage image = new BitmapImage();
-            image.SetSource(photo.Result.ChosenPhoto);
-            imgPhoto.Source = image;
-            }*/
-            /*   if (e.TaskResult == TaskResult.OK)
-            {
-            MessageBox.Show(e.ChosenPhoto.Length.ToString());
-            //Code to display the photo on the page in an image control named myImage.
-            //System.Windows.Media.Imaging.BitmapImage bmp = new System.Windows.Media.Imaging.BitmapImage();
-            //bmp.SetSource(e.ChosenPhoto);
-            //myImage.Source = bmp;
-            }*/
-            //this.NavigationService.Navigate(new Uri("/SelectedPicturePage.xaml", UriKind.RelativeOrAbsolute));
-            //this.NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.RelativeOrAbsolute));
             
-            //ContentPanel.Children.Clear();
             if (e.TaskResult == TaskResult.OK)
             {
                 BitmapImage image = new BitmapImage();
@@ -112,71 +80,10 @@ namespace StarSightings
                 ContentPanelScoop.Visibility = Visibility.Collapsed;
                 this.ApplicationBar.IsVisible = true;
             }
-            /*
-            Image selectedImage = new Image();
-            selectedImage.Height = 800;
-            selectedImage.Width = 480;
-            selectedImage.HorizontalAlignment = HorizontalAlignment.Center;
-            selectedImage.Stretch = Stretch.Uniform;
-            selectedImage.VerticalAlignment = VerticalAlignment.Center;
-            */
-
-            /*selectedImage.Source = image;
-            ContentPanel.Children.Add(selectedImage);
-            */
             
-            /*
-            Button DoneButton = new Button();
-            DoneButton.Height = 72;
-            DoneButton.Width = 160;
-            DoneButton.Margin = new Thickness(50,600,0,0);
-            DoneButton.VerticalAlignment = VerticalAlignment.Top;
-            DoneButton.HorizontalAlignment = HorizontalAlignment.Left;
-            //DoneButton.Foreground = new SolidColorBrush(Colors.Green);
-            DoneButton.Content = "accept";
-            DoneButton.Click += new RoutedEventHandler(onDoneButtonClick);
-
-            Button SAButton = new Button();
-            SAButton.Height = 72;
-            SAButton.Width = 220;
-            SAButton.Margin = new Thickness(226,600,0,0);
-            SAButton.VerticalAlignment = VerticalAlignment.Top;
-            SAButton.HorizontalAlignment = HorizontalAlignment.Left;
-            //SAButton.Foreground = new SolidColorBrush(Colors.Green);
-            SAButton.Content = "retake";
-            SAButton.Click += new RoutedEventHandler(onSAButtonClick);
-
-            ContentPanel.Children.Add(SAButton);
-            ContentPanel.Children.Add(DoneButton);
-            */
-           
-            //this.NavigationService.Navigate(new Uri("/ConfirmPictureSelected.xaml", UriKind.RelativeOrAbsolute));
         }
 
-        /*
-        public override void OnChooserReturn(object sender, EventArgs e)
-        {
-        base.OnChooserReturn(sender, e);
-        TaskEventArgs<PhotoResult> photo = e as TaskEventArgs<PhotoResult>;
-        if (photo != null && photo.Result != null && photo.Result.ChosenPhoto != null)
-        {
-        BitmapImage image = new BitmapImage();
-        image.SetSource(photo.Result.ChosenPhoto);
-        imgPhoto.Source = image;
-        }
-        }*/
-        /*
-        void onSAButtonClick(object sender, RoutedEventArgs e)
-        {
-            SelectPictureButton_Click(sender, e);
-        }
-
-        void onDoneButtonClick(object sender, RoutedEventArgs e)
-        {
-            //Goto scoop page; The image is in MainViewModel
-            this.NavigationService.Navigate(new Uri("/Scoop.xaml", UriKind.RelativeOrAbsolute));
-        }*/
-
+        
         void cameraCaptureTask_Completed(object sender, PhotoResult e)
         {
             if (e.TaskResult == TaskResult.OK)
@@ -210,44 +117,8 @@ namespace StarSightings
 
         private void noPictureButton_Click(object sender, RoutedEventArgs e)
         {
-            //this.NavigationService.Navigate(new Uri("/MySightings.xaml", UriKind.RelativeOrAbsolute));
-            //Trying the webserver :)
-            //App.SSAPI.TestSuggestion();
-//            System.Windows.MessageBox.Show("No picture taken. Con)
-            //this.NavigationService.Navigate(new Uri("/RadControlsWhereInput.xaml", UriKind.RelativeOrAbsolute));
-        }
-
-        /*
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            base.OnNavigatedTo(e);
-
- 
-                if (App.ViewModel.fromChooserTask)
-                {
-                    //Goto scoop page; The image is in MainViewModel
-                    App.ViewModel.fromChooserTask = false;
-                    this.NavigationService.Navigate(new Uri("/ConfirmPictureSelected.xaml", UriKind.RelativeOrAbsolute));
-                }
-
-                if (App.ViewModel.fromCameraTask)
-                {
-                    //Goto scoop page; The image is in MainViewModel
-                    App.ViewModel.fromCameraTask = false;
-                    this.NavigationService.Navigate(new Uri("/Scoop.xaml", UriKind.RelativeOrAbsolute));
-                }
-        } */
-          
-            /*
-            if (this.NavigationContext.QueryString.ContainsKey("ProductId"))
-            {
-                productID = this.NavigationContext.QueryString["ProductId"];
-            }
-            else
-            {
-                productID = App.Current.Resources["FeaturedProductID"].ToString();
-            }*/
-
+            
+        }        
 
         private void OnDetailsTap(object sender, System.Windows.Input.GestureEventArgs e)
         {
