1dd1e970c1383559ef49f473026a402b61f6c124
zhouzhin <zhouzhin@mobielement.com>
2013-03-18 08:15:20 +0000

    solve the login issues.

--- StarSightings/StarSightings/FacebookLoginPage.xaml.cs
+++ StarSightings/StarSightings/FacebookLoginPage.xaml.cs
@@ -25,7 +25,7 @@ namespace StarSightings
         /// For extensive list of available extended permissions refer to 
         /// https://developers.facebook.com/docs/reference/api/permissions/
         /// </remarks>
-        private const string ExtendedPermissions = "user_about_me,read_stream,publish_stream";
+        private const string ExtendedPermissions = "email, read_friendlists, user_subscriptions";//user_about_me,read_stream,publish_stream";
 
         private readonly FacebookClient _fb = new FacebookClient();
 
@@ -96,7 +96,7 @@ namespace StarSightings
                 var result = (IDictionary<string, object>)e.GetResultData();
                 var id = (string)result["id"];
 
-                
+                //LoginSS(accessToken);
                 //var url = string.Format("/Pages/FacebookInfoPage.xaml?access_token={0}&id={1}", accessToken, id);
 
                 //Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri(url, UriKind.Relative)));
@@ -110,7 +110,8 @@ namespace StarSightings
         private void LoginSS(string accessToken)
         {
             App.ViewModel.AccountType = Constants.ACCOUNT_TYPE_FACEBOOK;
-            string query = string.Format("fb_token={0}&device_id={1}&auto_register=1",accessToken, App.ViewModel.DeviceId);
+            //string query = string.Format("fb_token={0}&device_id={1}&auto_register=1",accessToken, App.ViewModel.DeviceId);
+            string query = string.Format("fb_token={0}&auto_register=1", accessToken);
             myLoginEventHandler = new LoginEventHandler(LoginCompleted);
             App.SSAPI.LoginHandler += myLoginEventHandler;
             App.SSAPI.Login(App.ViewModel.AccountType, query);
@@ -130,23 +131,34 @@ namespace StarSightings
                 if (App.ViewModel.AccountType == Constants.ACCOUNT_TYPE_DEVICE)
                     App.ViewModel.NeedLogin = true;
                 else*/
-                App.ViewModel.NeedLogin = false;
 
-                App.Config.UpdateAlerts();
+                App.Config.loginSuccess = true;
+                App.ViewModel.IsDataLoaded = false;
+                App.ViewModel.NeedLogin = false;
+                
+                App.Config.UpdateAlerts();                
                 /*
                 App.ViewModel.KeywordType = Constants.KEYWORD_MY;
                 App.ViewModel.SearchKeywordSearch(true, 0, null);
                 */
+                /*
                 NavigationService.RemoveBackEntry();
                 this.NavigationService.GoBack();                
+                 * */
+                GoHome(this, null);
             }
             else
             {
                 App.ViewModel.NeedLogin = true;
-                MessageBox.Show("Cannot login, please try again.");
+                //MessageBox.Show("Cannot login, please try again.");
                 NavigationService.RemoveBackEntry();
                 this.NavigationService.GoBack();
             }
+        }
+
+        private void GoHome(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/MainPage.xaml?clear", UriKind.RelativeOrAbsolute));
         }
     }
 }
